05d777bf9fa0cebf946f0ea5d80b09095cb3b1a2
Ebere Abanonu <eaba@users.noreply.github.com>
2019-03-19 11:22:28 +0100

    Add files via upload

---
+++ QueueLogger.cs
@@ -0,0 +1,90 @@
+﻿using MassTransit;
+using System;
+using Akka.Actor;
+using Akka.Event;
+using Akka.Configuration;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using v2.Message.Buses;
+using System.Collections.Immutable;
+namespace Akka.MassTransit.Logger
+{
+    public class QueueLogger : ReceiveActor
+    {
+        private ISendEndpoint _sendEndPoint;
+        private List<Dictionary<string, string>> _logCache;
+        private string queueUri = "";
+        public QueueLogger()
+        {
+            _logCache = new List<Dictionary<string, string>>();
+            ReceiveAsync<Debug>(async e => await LogAsync(e.LogLevel().ToString(), e.ToString(), Context.System.Name));
+            ReceiveAsync<Info>(async e => await LogAsync(e.LogLevel().ToString(), e.ToString(), Context.System.Name));
+            ReceiveAsync<Warning>(async e => await LogAsync(e.LogLevel().ToString(), e.ToString(), Context.System.Name));
+            ReceiveAsync<Error>(async e => await LogAsync(e.LogLevel().ToString(), e.ToString(), Context.System.Name));
+            Receive<InitializeLogger>(_=> Init(Sender));
+        }
+        protected override void PreStart()
+        {
+            var akkaConfig = Context.System.Settings.Config.GetConfig("akka");
+            queueUri = akkaConfig.GetString("queue-uri");
+            base.PreStart();
+        }
+
+        protected override void PostStop()
+        {
+            base.PostStop();
+        }
+        private async Task LogAsync(string level, string message, string system)
+        {
+            try
+            {
+                var data = new Dictionary<string, string> {
+                    {"Level", level},
+                    {"System", system },
+                    {"Log", message }
+                };
+                if(AkkaService.Bus != null) //Logging may start before bus is ready
+                {
+                    var dto = new Dto(data.ToImmutableDictionary());
+                    if (_sendEndPoint != null)
+                    {
+                        if (_logCache.Count > 0)
+                        {
+                            foreach (var log in _logCache)
+                            {
+                                await _sendEndPoint.Send(new Dto(log.ToImmutableDictionary()));
+                            }
+                            _logCache.Clear();
+                        }
+                        await _sendEndPoint.Send(dto);
+                    }
+                    else
+                    {
+                        _sendEndPoint = await AkkaService.Bus.GetSendEndpoint(new Uri(queueUri));
+                        if(_logCache.Count > 0)
+                        {
+                            foreach (var log in _logCache)
+                            {
+                                await _sendEndPoint.Send(new Dto(log.ToImmutableDictionary()));
+                            }
+                            _logCache.Clear();
+                        }       
+                        await _sendEndPoint.Send(dto);
+                    }
+                }
+                else
+                {
+                    _logCache.Add(data);
+                }
+                
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private void Init(IActorRef sender)
+        {            
+            sender.Tell(new LoggerInitialized());
+        }
+    }
+}
